d1b04ee982ed9b3ff93393d69e59d6e6cc1c68af
jeffbaleva <jeff.baleva45@gmail.com>
2023-10-18 02:57:22 -0400

    added files and photos

---
+++ scripts/index.js
@@ -0,0 +1,3 @@
+<script src="https://cdn.jsdelivr.net/npm/bootsrap@5.0.2/dist/js/bootsrap.bundle.min.js"
+integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+crossorigin="anonymous"></script>
